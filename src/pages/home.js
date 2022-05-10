6c70eb78bbc80e1c57495e9af7b4a2b998e8cf69
Carlos Seda <carlosseda123@gmail.com>
2022-05-10 12:31:34 -0500

    arngren inspo

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Home(){
+    return <div>
+    <h1>CreaSSAAAAAAr cuenta</h1>
+    <div style="text-align:center; padding: 50px 0px;">
+        <img style="width:300px;" src="../images/background.png" />
+        <p>holaaaaaaaaaaaaaaaaaaaa</p>
+    </div>   
+    </div>;
+}
+
+export default Home;
